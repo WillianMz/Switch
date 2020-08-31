fce72990571cfdfeecf8883e016ccbac9caebcd8
WillianMz <willianmazzorana@hotmail.com>
2020-08-31 07:42:54 -0300

    Mapeamento e FluentAPI

--- Switch/Switch.Infra.Data/Context/SwitchContext.cs
+++ Switch/Switch.Infra.Data/Context/SwitchContext.cs
@@ -1,16 +1,28 @@
 ﻿using Microsoft.EntityFrameworkCore;
 using Switch.Domain.Entities;
+using Switch.Infra.Data.Config;
 
 namespace Switch.Infra.Data.Context
 {
     public class SwitchContext : DbContext
     {
-        //conf. o mapeamento por convensão, as teabelas são criadas automaticamente pelo EF
+        //conf. o mapeamento por convensão, as tabelas são criadas automaticamente pelo EF
         public DbSet<Usuario> Usuarios { get; set; }
 
         public SwitchContext(DbContextOptions options) : base(options)
         {
 
         }
+
+        //FluentAPI
+        //precisar estar na classe que possui herança do DbContext
+        //Faz a sobreposição
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.ApplyConfiguration(new UsuarioConfiguration());
+
+            base.OnModelCreating(modelBuilder);
+        }
+
     }
 }
